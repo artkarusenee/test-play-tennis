aaf6e0a9878c1429140fd29b1ee41090fc657741
art_karusenee <art@paysbuy.com>
2017-07-08 12:16:24 +0700

    create app for running game

--- src/main/java/com/tennis/App.java
+++ src/main/java/com/tennis/App.java
@@ -1,13 +1,41 @@
 package com.tennis;
 
+import com.tennis.playrecord.data.DefaultGameTennisPlayRecordDataReader;
+import com.tennis.playrecord.data.GameTennisPlayRecord;
+import com.tennis.playrecord.data.GameTennisPlayRecordDataReadable;
+import com.tennis.playrecord.result.DefaultGameTennisPlayRecordShow;
+import com.tennis.playrecord.result.GameTennisPlayRecordShowable;
+
 /**
- * Hello world!
+ * App for running Game Tennis
  *
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+        if(args.length ==  0) {
+            System.out.println("Please specify input file path");
+            System.exit(1);
+        }
+
+        String path = args[0];
+
+        if(path != null){
+
+            try {
+                GameTennisPlayRecordDataReadable reader = new DefaultGameTennisPlayRecordDataReader();
+                GameTennisPlayRecord record = reader.load(path);
+
+                GameTennisPlayRecordShowable showing = new DefaultGameTennisPlayRecordShow();
+                showing.show(record);
+
+            }
+            catch (Exception ex){
+                System.out.println(ex.getMessage());
+            }
+        }
+
     }
 }
